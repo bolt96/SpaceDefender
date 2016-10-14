aeea1eeb42b92f6fbbc024f145ee5f5277791225
TestSubject8 <agkatreya@gmail.com>
2016-10-14 20:15:00 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(int agrv, char *argv[])
+{
+	//file open
+	//file write (to screen - see `cat` implementation)
+	//start timer
+	//
+	return 0;
+}
